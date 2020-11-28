2f5c32106c9b22122b273f062ab56a92762bb72e
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-11-28 23:54:18 +1300

    Default

---
+++ layouts/error.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex my-0 mx-auto min-h-screen items-center justify-center text-center">
+    <NuxtLink class="border-red-500 text-white rounded-md px-5 py-2 m-2  select-none dark:bg-red-600 bg-blue-600 focus:outline-none focus:shadow-outline" to="/">Return</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="dark:bg-black bg-gray-200 flex items-center text-center justify-center my-0 mx-auto min-h-screen">
+    <div>
+      <Logo />
+    </div>
+  </div>
+</template>
